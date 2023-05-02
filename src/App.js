d82b7f96ebd52d8498d29b7ec92cdf818bb31f9f
Oignnon - <td665pro@pm.me>
2023-05-02 08:33:27 +0200

    Die faces created with random numbers

--- src/App.js
+++ src/App.js
@@ -1,10 +1,29 @@
 import React from "react";
+import Die from "./Die";
+
+
 
 export default function App() {
+  const [dice, setDice] = React.useState(allNewDice())
+
+  function allNewDice() {
+    const newDice = []
+    for (let i = 0; i < 10; i++) {
+    newDice.push(Math.ceil(Math.random() * 6))
+    }
+    return newDice
+  }
+
+  const diceElements = dice.map(die => <Die value={die} />)
+
   return (
-    <div className="App">
-      <h1>Hi</h1>
-    </div>
+    <main>
+        <div className="game-ui">
+          <div className="boxes">
+            {diceElements}
+          </div>
+      </div>
+    </main>
   );
 }
 
